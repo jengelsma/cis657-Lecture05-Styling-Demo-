63984bdb1b6eee86ccad08e9e5588f07a4beaedb
Jonathan Engelsma <jengelsma@gmail.com>
2021-05-20 13:58:34 -0400

    Inital commit.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Calculator from "./screens/Calculator";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Calculator />
       <StatusBar style="auto" />
     </View>
   );
